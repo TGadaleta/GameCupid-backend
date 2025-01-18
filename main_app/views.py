f3864772fd8ce82044190ac1aa6b2dc05932d7ee
Tony Gadaleta <anthonygadaleta@gmail.com>
2025-01-18 12:03:50 -0500

    codefix

--- main_app/views.py
+++ main_app/views.py
@@ -4,5 +4,5 @@ from rest_framework.response import Response
 # Define the home view
 class Home(APIView):
   def get(self, request):
-    content = {'message': 'Welcome to the cat-collector api home route!'}
+    content = {'message': 'Welcome to the gamecupid api home route!'}
     return Response(content)
