2aa5af70b22c9ba13bd216fbaa1ffb7a164f02d8
Tony Gadaleta <anthonygadaleta@gmail.com>
2025-01-19 22:26:48 -0500

    updated ProfileSerializer

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,10 +1,33 @@
 from rest_framework import serializers
 from .models import (Profile, Profile_Match, Profile_BLock, Game, Platform, Genre_Scores, )
+from django.contrib.auth.models import User
 
 class ProfileSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username')
+    email = serializers.EmailField(source='user.email')
+    password = serializers.CharField(source='user.password', write_only=True)
     class Meta:
         model = Profile
         fields = '__all__'
+    
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user = User.objects.create_user(**user_data)
+        profile = Profile.objects.create(user=user, **validated_data)
+        return profile
+    
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop('user', None)
+        if user_data:
+            for attr, value in user_data.items():
+                setattr(instance.user, attr, value)
+            instance.user.save()
+
+    def delete(self, instance):
+        user = instance.user
+        instance.delete()
+        user.delete
+        
 
 class Profile_MatchSerializer(serializers.ModelSerializer):
     class Meta:
