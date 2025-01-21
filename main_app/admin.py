2f4159ad75c7c6228433d2eabc372d50a2e1b69e
Tony Gadaleta <anthonygadaleta@gmail.com>
2025-01-21 12:45:57 -0500

    fix: correct class name typos and update model relationships in admin and serializers feat: add new views for profile games, platforms, matches, and blocks management refactor: adjust model options and field types for better data integrity

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Profile, Profile_BLock, Profile_Match, Game, Platform, Genre_Scores
+from .models import Profile, Profile_Block, Profile_Match, Game, Platform, Genre_Scores
 
 admin.site.register(Profile)
-admin.site.register(Profile_BLock)
+admin.site.register(Profile_Block)
 admin.site.register(Profile_Match)
 admin.site.register(Game)
 admin.site.register(Platform)
