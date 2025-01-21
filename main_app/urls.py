11f2c38095bf56120f4a7f7ae5e7eaa3b3f9e1ff
Jose Salcedo <Jsalcedo19@gmail.com>
2025-01-21 10:06:01 -0500

    add delete routes for games, platforms, matches, and blocs; add edit routes for game, platform, and genre scores

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,6 +9,23 @@ urlpatterns = [
     path('profile/games/', ProfileGamesList.as_view(), name='profile-games'),
     path('profile/platforms/', ProfilePlatformsList.as_view(), name='profile-platforms'),
     path('profile/games/edit', ProfileGamesEdit.as_view(), name='profile-games-edit'),
-    path('profile/platforms/edit', ProfilePlatforms/edit.as_view(), name='profile-platforms-edit'),
+    path('profile/platforms/edit', ProfilePlatformsEdit.as_view(), name='profile-platforms-edit'),
+    path('profile/games/delete', ProfileGamesDelete.as_view(), name='profile-games-delete'),
+    path('profile/platforms/delete', ProfilePlatformsDelete.as_view(), name='profile-platforms-delete'),
+    path('profile/matches/', ProfileMatchesList.as_view(), name='profile-matches'),
+    path('profile/matches/delete', ProfileMatchesDelete.as_view(), name='profile-matches-delete'),
+    path('profile/blocs/', ProfileBlocsList.as_view(), name='profile-blocs'),
+    path('profile/blocs/delete', ProfileBlocsDelete.as_view(), name='profile-blocs-delete'),
+    path('game/', Game.as_view(), name='game'),
+    path('game/edit/', GameEdit.as_view(), name='game-edit'),
+    path('game/delete/', GameDelete.as_view(), name='game-delete'),
+    path('platform/', Platform.as_view(), name='platform'),
+    path('platform/edit/', PlatformEdit.as_view(), name='platform-edit'),
+    path('platform/delete/', PlatformDelete.as_view(), name='platform-delete'),
+    path('genre-scores/', GenreScores.as_view(), name='genre-scores'),
+    path('genre-scores/edit/', GenreScoresEdit.as_view(), name='genre-scores-edit'),
+
+
+
 
 ]
